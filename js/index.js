c2d2b7c23319c346bf8ef575d8af44ef2f2cbd85
Louis-Marie Dumigron <louis-mariedumigron@orange.fr>
2017-01-25 10:15:53 +0100

    Ajout Fichiers : Paralax/Index.html/CSS/JS

---
+++ js/index.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+
+	var parallaxBox = document.getElementById ( 'box' );
+	var c1left = document.getElementById ( 'l1' ).offsetLeft,
+	c1top = document.getElementById ( 'l1' ).offsetTop,
+	c2left = document.getElementById ( 'l2' ).offsetLeft,
+	c2top = document.getElementById ( 'l2' ).offsetTop;
+	
+	parallaxBox.onmousemove = function ( event ) {
+		event = event || window.event;
+		var x = event.clientX - parallaxBox.offsetLeft,
+		y = event.clientY - parallaxBox.offsetTop;
+		
+		mouseParallax ( 'l1', c1left, c1top, x, y, 10 );
+		mouseParallax ( 'l2', c2left, c2top, x, y, 45 );
+	}
+	
+}
+
+function mouseParallax ( id, left, top, mouseX, mouseY, speed ) {
+	var obj = document.getElementById ( id );
+	var parentObj = obj.parentNode,
+	containerWidth = parseInt( parentObj.offsetWidth ),
+	containerHeight = parseInt( parentObj.offsetHeight );
+	obj.style.left = left - ( ( ( mouseX - ( parseInt( obj.offsetWidth ) / 2 + left ) ) / containerWidth ) * speed ) + 'px';
+	obj.style.top = top - ( ( ( mouseY - ( parseInt( obj.offsetHeight ) / 2 + top ) ) / containerHeight ) * speed ) + 'px';
+}
